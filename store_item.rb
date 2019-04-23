1d1d222b072df1aa85939a31bce3f7ffad9131f0
ThundaHorse <abeeeeekim@gmail.com>
2019-04-23 11:26:33 -0500

    made 3 objects using js and ruby syntax

---
+++ store_item.rb
@@ -0,0 +1,18 @@
+item1 = {
+  "item" => "Lays Potato Chips", 
+  "price" => 4, 
+  "color" => "Yellow" 
+}
+
+item2 = {
+  item: "Fruit by the foot", 
+  price: 2, 
+  color: "multi-color" 
+}
+
+item3 = {
+  "item" => "Mountin Dew", 
+  "price" => 2, 
+  "color" => "green" 
+}
+
